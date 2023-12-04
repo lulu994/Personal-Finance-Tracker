10a02ae05b049277b21121b09cb45a39f48a5941
lulu994 <loubna.neddah@gmail.com>
2023-12-04 15:23:13 +0100

    adding files

--- personal-finance-tracker-backend/server.js
+++ personal-finance-tracker-backend/server.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
-const app = express();
 
 require('dotenv').config();
 dotenv.config();
@@ -18,12 +17,18 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/your-database-name'), {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  // Remove the following line, as it is no longer needed and is deprecated
-  // useFindAndModify: false,
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+  }
 };
+connectDB(); // Assuming this function sets up the MongoDB connection
 
 // Define mongoose models and schemas
 const User = mongoose.model('User', {
@@ -84,7 +89,6 @@ app.get('/dashboard', authenticateToken, (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
